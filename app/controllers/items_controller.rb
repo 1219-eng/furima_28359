8866f8db880a70aa2177b9570afdf6bc4175b10c
1219-eng <r.captain1219@icloud.com>
2020-08-20 17:58:45 +0900

    ログインログアウトの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,16 @@
 class ItemsController < ApplicationController
-  def index
+
+  def create
   end
+  def new
+    current_item.update(item_params)
+  end
+
+  private
+
+  def itemr_params
+    params.require(:user).permit(:name, :email)
+  end
+
+    # redirect_to ""
 end
--- db/migrate/20200818073040_devise_create_users.rb
+++ db/migrate/20200818073040_devise_create_users.rb
@@ -4,16 +4,22 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      t.string :email,              null: false, default:""
+      t.string :encrypted_password, null: false, default:""
+      t.string :nickname,           null: false, default:""
+      t.date   :birthday,           null: false
+      t.string :family_name,        null: false, default:""
+      t.string :name,               null: false, default:""
+      t.string :phonetic_name,      null: false, default:""
+      t.string :phonetic_family_name,null: false, default:""
 
+      
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
 
       ## Rememberable
-      t.datetime :remember_created_at
-      
+      t.datetime :remember_created_at 
       ## Trackable
       # t.integer  :sign_in_count, default: 0, null: false
       # t.datetime :current_sign_in_at
